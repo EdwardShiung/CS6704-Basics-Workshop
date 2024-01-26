b99573ece59bbd8f809f98d017eaa16a778e91e1
root <root@DESKTOP-URA5I64.localdomain>
2024-01-26 12:47:45 -0500

    Update the Roman file

--- Roman.java
+++ Roman.java
@@ -2,6 +2,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+    /*
+     * Function Name: intToRoman
+     * Description: The function will receive an integer as an input and produce a roman numeral.
+     * Author: Minhyuk Ko
+     * Student ID: minhyukko
+     * Code Editor: Vim
+     */
+    public static String intToRoman(int num) {
+        int[] values = { 1000, 500, 100, 50, 10, 5, 1 };
+        String[] symbols = { "M", "D", "C", "L", "X", "V", "I" };
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
 
     /*
      * Function Name: romanToInt Function
