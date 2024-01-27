60ebf80a30afbc31be8f9e4f12cbfb6687c04c4a
EdwardShiung <edwardshiung@gmail.com>
2024-01-26 22:12:18 -0500

    Update the scripts file

--- scripts.sh
+++ scripts.sh
@@ -2,8 +2,29 @@
 
 # retrive name data from github
 data_repos=$(./githubapi-get.sh $GITHUBTOKEN /users/EdwardShiung/repos | grep '"name":' | sort)
-echo $data_repos > repos.json
-
-# retrive name data from github
+# retrive commits data from github
 data_commits=$(./githubapi-get.sh $GITHUBTOKEN /repos/CS6704-VT/Basics-Workshop/commits | grep '"message":')
-echo $data_commits > commits.json
+
+
+# Organize date to JSON
+# Begin
+echo "{" > repos.json
+echo $data_repos | while IFS= read -r line;
+
+do
+    echo "  $line" >> repos.json
+done
+# End
+echo "}" >> repos.json
+
+
+# Organize date to JSON
+# Begin
+echo "{" > commits.json
+echo $data_commits | while IFS= read -r line;
+
+do
+    echo "  $line" >> commits.json
+done
+# End
+echo "}" >> commits.json
